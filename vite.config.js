f7a86404d57d3b18bd458a3241ffaef26bc23e69
Callum-OP <callumoprey@gmail.com>
2026-05-28 19:59:48 +0100

    Added workflow for deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/Video-Pose-Finder/',
   plugins: [react(), tailwindcss()],
   optimizeDeps: {
     exclude: ['@mediapipe/tasks-vision'],
